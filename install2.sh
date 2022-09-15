c3f9f8537c3c5b2b939c50d28bfb75d0cdf786c7
meCharo <mecharo21@icloud.com>
2022-09-15 16:09:46 +0800

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -18,7 +18,7 @@ systemctl enable dhcpcd
 # 4. bootloader grub
 pacman -S grub efibootmgr
 mkdir /boot/efi
-mount /dev/sda2 /boot/efi
+mount /dev/sdb2 /boot/efi
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
